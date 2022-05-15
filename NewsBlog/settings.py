4016d235010e25f4750e1d9a77c79380f085a594
jagdish patel <dhruvmpatel096@gmail.com>
2022-05-15 16:54:23 +0530

    15/5(8)

--- NewsBlog/settings.py
+++ NewsBlog/settings.py
@@ -135,6 +135,8 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
