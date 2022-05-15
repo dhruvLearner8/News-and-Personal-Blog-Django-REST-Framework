a4a18a70dd775aa773004bf5fca18eeaf4e1cbbc
jagdish patel <dhruvmpatel096@gmail.com>
2022-05-15 17:08:39 +0530

    15/5(11)

--- NewsBlog/settings.py
+++ NewsBlog/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
